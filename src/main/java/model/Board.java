06ccfe6ade9ccb128d7bd0116f821c6529859e2f
HADDAG Amayas <amayas.haddag@etu.u-paris.fr>
2024-01-31 22:23:02 +0100

    Some updates for Board.java class

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -19,99 +19,99 @@ public class Board {
     
     private Random random;
 
-    // public Board() {
-    //     this.grid = new Cell[BOARD_WIDTH][BOARD_HEIGHT];
-    //     this.remainingStocks = initialStocks();
-    //     this.remainingCells = initialCells();
-    //     this.random = new Random();
-    // }
+    public Board() {
+        this.grid = new Cell[BOARD_WIDTH][BOARD_HEIGHT];
+        this.remainingStocks = initialStocks();
+        this.remainingCells = initialCells();
+        this.random = new Random();
+    }
 
-    // /**
-    //  * This function is only used to intialize the variable remainingStocks in constructor
-    //  * @return returns initial stocks associated to each corporation
-    //  */
-    // private Map<Corporation, Integer> initialStocks() {
-    //     Map<Corporation, Integer> startingStocks = new HashMap<>();
-    //     for (Corporation corporation : Corporation.allCorporations()) {
-    //         startingStocks.put(corporation, INITIAL_STOCKS_PER_COMPANY);
-    //     }
-    //     return startingStocks;
-    // }
-
-
-    // /**
-    //  * This function is only used to initialize the cells set in the constructor
-    //  * @return returns all the cells of the board in a list
-    //  */
-    // private List<Point> initialCells() {
-    //     List<Point> cells = new LinkedList<>();
-    //     for (int i = 0; i < BOARD_WIDTH; i++) {
-    //         for (int j = 0; j < BOARD_HEIGHT; j++) {
-    //             Point cell = new Point(i, j);
-    //             cells.add(cell);
-    //         }
-    //     }
-    //     return cells;
-    // }
-
-    // /**
-    //  * 
-    //  * @param amount is the amount of cells we want to check
-    //  * @return whether the number of remaining cells is greater or equals the amount given
-    //  */
-    // public boolean enoughRemainingCells(int amount) {
-    //     return amount <= this.remainingCells.size();
-    // }
+    /**
+     * This function is only used to intialize the variable remainingStocks in constructor
+     * @return returns initial stocks associated to each corporation
+     */
+    private Map<Corporation, Integer> initialStocks() {
+        Map<Corporation, Integer> startingStocks = new HashMap<>();
+        for (Corporation corporation : Corporation.allCorporations()) {
+            startingStocks.put(corporation, INITIAL_STOCKS_PER_COMPANY);
+        }
+        return startingStocks;
+    }
 
 
-    // /**
-    //  * 
-    //  * @return a random cell from the remaining cells list
-    //  */
-    // public Point getFromRemainingCells() {
-    //     int index = this.random.nextInt(remainingCells.size());
-    //     Point cell = remainingCells.remove(index);
-    //     return cell;
-    // }
+    /**
+     * This function is only used to initialize the cells set in the constructor
+     * @return returns all the cells of the board in a list
+     */
+    private List<Point> initialCells() {
+        List<Point> cells = new LinkedList<>();
+        for (int i = 0; i < BOARD_WIDTH; i++) {
+            for (int j = 0; j < BOARD_HEIGHT; j++) {
+                Point cell = new Point(i, j);
+                cells.add(cell);
+            }
+        }
+        return cells;
+    }
 
-    // /**
-    //  *
-    //  * @param corporation the corporation we want to know the size of
-    //  * @return given a corporation, it returns its size on the board
-    //  */
-    // public int getCorporationSize(Corporation corporation) {
-    //     int size = corporationSizes.get(corporation);
-    //     return size;
-    // }
+    /**
+     * 
+     * @param amount is the amount of cells we want to check
+     * @return whether the number of remaining cells is greater or equals the amount given
+     */
+    public boolean enoughRemainingCells(int amount) {
+        return amount <= this.remainingCells.size();
+    }
 
-    // /**
-    //  * 
-    //  * @param corporation
-    //  * @return whether the given corporation is safe or not
-    //  */
-    // public boolean corporationIsSafe(Corporation corporation) {
-    //     return getCorporationSize(corporation) >= SAFETY_SIZE;
-    // }
 
-    // /**
-    //  * 
-    //  * @param cell
-    //  * @return all the adjacent cells to the given cell
-    //  */
-    // private List<Point> adjacentCells(Point cell) {
-    //     List<Point> adjacentCells = new LinkedList<>();
-    //     for (int i = -1; i <= 1; i++) {
-    //         for (int j = -1; j <= 1; j++) {
-    //             if (i != 0 || j != 0) {
-    //                 Point adjacent = new Point(i + cell.getX(), j + cell.getY());
-    //                 if (adjacent.isInBounds(BOARD_WIDTH, BOARD_HEIGHT)) {
-    //                     adjacentCells.add(adjacent);
-    //                 }
-    //             }
-    //         }
-    //     }
+    /**
+     * 
+     * @return a random cell from the remaining cells list
+     */
+    public Point getFromRemainingCells() {
+        int index = this.random.nextInt(remainingCells.size());
+        Point cell = remainingCells.remove(index);
+        return cell;
+    }
 
-    //     return adjacentCells;
-    // }
+    /**
+     *
+     * @param corporation the corporation we want to know the size of
+     * @return given a corporation, it returns its size on the board
+     */
+    public int getCorporationSize(Corporation corporation) {
+        int size = corporationSizes.get(corporation);
+        return size;
+    }
+
+    /**
+     * 
+     * @param corporation
+     * @return whether the given corporation is safe or not
+     */
+    public boolean corporationIsSafe(Corporation corporation) {
+        return getCorporationSize(corporation) >= SAFETY_SIZE;
+    }
+
+    /**
+     * 
+     * @param cell
+     * @return all the adjacent cells to the given cell
+     */
+    private List<Point> adjacentCells(Point cell) {
+        List<Point> adjacentCells = new LinkedList<>();
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i != 0 || j != 0) {
+                    Point adjacent = new Point(i + cell.getX(), j + cell.getY());
+                    if (adjacent.isInBounds(BOARD_WIDTH, BOARD_HEIGHT)) {
+                        adjacentCells.add(adjacent);
+                    }
+                }
+            }
+        }
+
+        return adjacentCells;
+    }
 
 }
