87b5ecca17d231aeab24cd85684712d52cc00cbc
DECK Arthur <arthur.deck@etu-u.paris.fr>
2024-02-01 09:02:24 +0100

    test pour la vue isometric

---
+++ src/main/java/view/Debug.java
@@ -0,0 +1,8 @@
+package view;
+
+public class Debug {
+    public static void main(String[] args) {
+        GameView g = new GameView(null);
+        g.setVisible(true);
+    }
+}
--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -1,5 +1,59 @@
 package view;
 
-public class GameView {
-    
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import control.GameController;
+
+public class GameView extends JFrame {
+
+    public GameView(GameController controller) {
+        this.controller = controller;
+        this.map = new MapView();
+
+        setTitle("Acquire");
+        setSize(1200, 900);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        add(map);
+    }
+
+    GameController controller;
+    MapView map;
+
+    final int SIZE = 12;
+
+    class MapView extends JPanel {
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+
+            AffineTransform at = new AffineTransform();
+            at.rotate(Math.toRadians(45));
+            at.scale(1,0.5);
+            g2d.setTransform(at);
+
+            System.out.println("Rotationnnnnnnnnnnnnn");
+
+            int cellWidth = getWidth() / SIZE;
+            int cellHeight = getHeight() / SIZE;
+
+            for (int row = 0; row < SIZE; row++) {
+                int y = row * cellHeight;
+                for (int col = 0; col < SIZE; col++) {
+                    int x = col * cellWidth;
+                    g.drawRect(x, y, cellWidth, cellHeight);
+                }
+            }
+
+            g2d.dispose();
+        }
+    }
 }
