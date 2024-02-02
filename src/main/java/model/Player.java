87fca16033bdba2cce55644cd830daa9c1fb5bbb
igor <igor2004444@gmail.com>
2024-02-03 00:17:31 +0100

    #1 implementation of Player class

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,5 +1,40 @@
 package model;
 
+import java.util.HashMap;
+
 public class Player {
-    
+    private int cash;
+    private HashMap<Corporation,Integer> earnedStocks;
+    private Point[] deck;
+
+    public HashMap<Corporation,Integer> getEarnedStocks(){
+        return this.earnedStocks;
+    }
+    public Point[] getDeck(){
+        return this.deck;
+    }
+    public void setDeck(Point[] newDeck){
+        this.deck = newDeck;
+    }
+    public boolean hasEnoughCash(int amount){
+        return this.cash >= amount;
+    }
+    public void addToCash(int amount){
+        this.cash+=amount;
+    }
+    public void removeFromCash(int amount){
+            this.cash-=amount;
+    }
+    public boolean hasEnoughStocks(Corporation corp,int amount){
+        return earnedStocks.get(corp) >= amount;
+    }
+    public void addToEarnedStocks(Corporation corp,int amount){
+        this.earnedStocks.put(corp, amount);
+    }
+    public void removeToEarnedStocks(Corporation corp,int amount){
+        this.cash-=amount;
+    }
+    public Point getCell(int index){
+        return this.deck[index];
+    }
 }
