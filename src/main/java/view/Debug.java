d17d3804f73effe8833fc7edfa8e47a13fd75ee9
Nida HAMMOUCHE <nida.hammouche@etu.u-paris.fr>
2024-02-19 16:25:44 +0100

    Implemented MenuView.java class

--- src/main/java/view/Debug.java
+++ src/main/java/view/Debug.java
@@ -1,9 +1,15 @@
 package view;
 
+import view.Menu.MenuView;
+
 public class Debug {
     public static void main(String[] args) {
-        GameFrame g = new GameFrame (null, null);
+        /* GameFrame g = new GameFrame (null, null);
         g.setVisible(true);
-        g.repaint();
+        g.repaint(); */
+        GameFrame h = new GameFrame(null, null); // Créer une instance de GameFrame
+        h.setForm(new MenuView()); // Ajouter une instance de MenuView à GameFrame
+        h.setVisible(true); // Rendre GameFrame visible
+        
     }
 }
