e6cd1daa84ca417cf5a4d4dc5fc30cfa42e0fe55
HADDAG Amayas <amayas.haddag@etu.u-paris.fr>
2024-02-20 21:10:23 +0100

    Corrected a compilation bug

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -9,12 +9,6 @@ import model.Player;
 
 public class App {
     public static void main(String[] args) {
-        Board board = new Board();
-        List<Player> players = new LinkedList<>();
-        Player p1 = new Player();
-        players.add(p1);
 
-        GameController controller = new GameController(board, null, players);
-        controller.gameLoop();
     }
 }
