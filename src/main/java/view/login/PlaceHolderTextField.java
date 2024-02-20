fa780dc5ee57cd0fd73c7bfa24a057d9a46cc2b2
igor <igor2004444@gmail.com>
2024-02-20 15:12:16 +0100

    #9 -

--- src/main/java/view/login/PlaceHolderTextField.java
+++ src/main/java/view/login/PlaceHolderTextField.java
@@ -3,27 +3,33 @@ package view.login;
 import com.formdev.flatlaf.extras.components.FlatTextArea;
 import com.formdev.flatlaf.extras.components.FlatTextField;
 
+import java.awt.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
 public class PlaceHolderTextField extends FlatTextField implements FocusListener {
-    public String text;
+    private String text;
+
     private boolean alreadyWritten = false;
 
     public PlaceHolderTextField(String text){
         this.text=text;
         this.addFocusListener(this);
+        this.setForeground(Color.GRAY);
     }
     @Override
     public void focusGained(FocusEvent e){
-        if (this.text == InterfaceLoginMessages.PASSWORD_TEXT_AREA || this.text == InterfaceLoginMessages.ID_TEXT_AREA){
+        if (this.text.equals(this.getText()) || this.text.equals(this.getText())){
+            System.out.println("Je suis rentré ici");
             this.setText("");
+            setForeground(Color.WHITE);
         }
     }
     @Override
     public void focusLost(FocusEvent e){
-        if (this.text ==  ""){
+        if (this.getText().isEmpty()){
             this.setText(text);
+            setForeground(Color.GRAY);
         }
     }
 }
