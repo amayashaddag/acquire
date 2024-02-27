fbcc12163c088f0b0cbec234ff67c8ee3affb173
Nida HAMMOUCHE <nida.hammouche@etu.u-paris.fr>
2024-02-27 13:39:26 +0100

    Started implementing database reading

---
+++ src/main/java/control/database/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package control.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/DataBase";
+    private static final String USERNAME = "login";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
