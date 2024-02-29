9b2bc8ae445cb6244b5870398b3c255b87919367
HADDAG Amayas <amayas.haddag@etu.u-paris.fr>
2024-02-29 21:25:22 +0100

    Added all the necessary dependencies for Firebase API

---
+++ src/main/java/control/auth/FirebaseInitializer.java
@@ -0,0 +1,42 @@
+package control.auth;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserRecord;
+
+import java.io.FileInputStream;
+
+public class FirebaseInitializer {
+
+    public static final String FIREBASE_CREDENTIALS_FILE_PATH = "src/main/ressources/auth/firebase-credentials.json";
+    public static void initialize() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream(FIREBASE_CREDENTIALS_FILE_PATH);
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+
+            UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                    .setEmail("lynaispretty@gmail.com")
+                    .setPassword("Lyna2024");
+
+            UserRecord userRecord = auth.createUser(request);
+
+        } catch (Exception e) {
+            // TODO : Arthur should handle this in GUI
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        initialize();
+    }
+}
