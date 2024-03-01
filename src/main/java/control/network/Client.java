842810d934f4e71ecd4b973e99f95e14440ed1fa
igor <igor2004444@gmail.com>
2024-03-01 16:01:57 +0100

    #19 Beginnong of implementation of Client class

--- src/main/java/control/network/Client.java
+++ src/main/java/control/network/Client.java
@@ -1,4 +1,26 @@
 package control.network;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
+    private boolean leave = false;
+    public static void main(String[] args)throws IOException {
+        BufferedReader serverRequest;
+        BufferedWriter clientRequest;
+        Socket socketOfClient;
+        try{
+            socketOfClient = new Socket("localhost",9999);
+            serverRequest = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
+            clientRequest = new BufferedWriter(new OutputStreamWriter(socketOfClient.getOutputStream()));
+
+        }catch (IOException e){
+            System.out.println("Can't connect to server");
+        }
+        try{
+
+        }
+
+
+    }
 }
