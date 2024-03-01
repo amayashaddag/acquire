6966d6a13387ba1949288c9b16415bd622313e44
igor <igor2004444@gmail.com>
2024-03-01 15:37:11 +0100

    #19 first version of Server Class

---
+++ src/main/java/control/network/Client.java
@@ -0,0 +1,4 @@
+package control.network;
+
+public class Client {
+}
--- src/main/java/control/network/Server.java
+++ src/main/java/control/network/Server.java
@@ -1,13 +1,63 @@
 package control.network;
+import java.io.*;
 import java.net.Socket;
 import java.net.ServerSocket;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import java.util.ArrayList;
 
 public class Server {
-    public static void main(String[] args){
+    public static void main(String[] args)throws IOException{
+        ServerSocket listener = null;
+        //ArrayList<Client> clientHandler = new ArrayList<>();
+        int client = 0;
+        try{
+            listener = new ServerSocket(9999);
+        }catch (IOException e){
+            e.printStackTrace();
+            System.exit(4);
+        }
+        try{
+            while(true){
+                Socket socketForClient = listener.accept();
+                //
+                System.out.println("Player n°"+client+" joined the game.");
+                client++;
+            }
+        }finally {
+            listener.close();
+        }
 
+    }
+    public static class ServiceThread extends Thread{
+        Socket clientSocket;
+        int idClient;
+        public ServiceThread(int idClient,Socket clientSocket){
+            this.idClient = idClient;
+            this.clientSocket = clientSocket;
+        }
+        @Override
+        public void run(){
+            try{
+                BufferedReader clientRequest = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter serverRequest = new BufferedWriter((new OutputStreamWriter(clientSocket.getOutputStream())));
+                while(true){
+                    serverRequest.write("Hello ! Welcome to you player n°"+idClient+" !");
+                    serverRequest.flush();
+                    String clientResponse = clientRequest.readLine();
+                    System.out.println(clientResponse);
+                    if(clientResponse.equals("LEAVE")){
+                        serverRequest.write("Thanks for playing see you soon !");
+                        serverRequest.flush();
+                        clientRequest.close();
+                        serverRequest.close();
+                        clientSocket.close();
+                        break;
+                    }
+
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+                System.exit(4);
+            }
+        }
     }
 }
