52666dbddf6bfd08d143c53040a3ca800717c367
igor <igor2004444@gmail.com>
2024-03-01 14:33:55 +0100

    #19 Server class beginning of implementation

--- src/main/java/Reseau/Client.java
+++
@@ -1,4 +0,0 @@
-package Reseau;
-
-public class Client {
-}
---
+++ src/main/java/control/network/Server.java
@@ -0,0 +1,13 @@
+package control.network;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+
+public class Server {
+    public static void main(String[] args){
+
+    }
+}
