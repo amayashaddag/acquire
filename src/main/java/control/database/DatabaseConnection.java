54da64e3141e8c2afa2265a1f64ad0caf1a8f9a5
igor <igor2004444@gmail.com>
2024-03-15 13:59:22 +0100

    Structure for database class

--- src/main/java/control/database/DatabaseConnection.java
+++ src/main/java/control/database/DatabaseConnection.java
@@ -2,9 +2,14 @@ package control.database;
 
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
+import model.Player;
 
 public class DatabaseConnection {
     private static final Firestore database = FirestoreClient.getFirestore();
+
+    public void addPlayer(String gameId, Player player){
+
+    }
 
 
 }
