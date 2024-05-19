c41126a959fb0f83670580fef883a3d1835914c1
DECK Arthur <arthur.deck@etu-u.paris.fr>
2024-05-19 21:40:06 +0200

    app

--- src/main/java/app/launcher/App.java
+++ src/main/java/app/launcher/App.java
@@ -1,6 +1,42 @@
 
 package app.launcher;
 
-public class App {
+import java.awt.Graphics;
 
+import javax.swing.SwingUtilities;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
+
+import control.firebaseinit.FirebaseClient;
+import control.menu.MenuController;
+import view.window.Form;
+import view.window.GameFrame;
+
+public class App {
+    public static void main(String[] args) {
+        FirebaseClient.initialize();
+        FlatDarculaLaf.setup();
+        GameFrame frame = GameFrame.currentFrame;
+        GameFrame.setForm(new Form() {
+            @Override
+            public void setOn(GameFrame g) {
+                g.setContentPane(this);
+                g.revalidate();
+                g.repaint();
+            }
+            @Override
+            protected void paintComponent(Graphics g) {
+                g.drawImage(view.assets.MenuResources.MImage.LOADING_BACKGROUND,
+                    0, 0, GameFrame.currentFrame.getWidth(),
+                    GameFrame.currentFrame.getHeight(), null);
+            }
+        });
+        SwingUtilities.invokeLater(() -> frame.setVisible(true));
+        try {
+            MenuController menuController = new MenuController();
+            menuController.start();
+        } catch (Exception e) {
+            GameFrame.showError(e, frame::dispose);
+        }
+    }
 }
